0275fd4a9603e45489598991819cb6d05811b0ab
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-12-11 13:22:10 -0800

    Add paris module for output formatting

--- aoc2020/src/main.rs
+++ aoc2020/src/main.rs
@@ -4,6 +4,8 @@ use aoc2020::logic::router::route_puzzle;
 fn main() {
     let matches = get_matches();
 
+    paris::log!("<blue>Advent Of Code 2020 CLI</>\n");
+
     let day = matches.value_of("day").unwrap().parse::<i32>().unwrap();
     let part = matches.value_of("part").unwrap().parse::<i32>().unwrap();
     let mut files = matches.values_of("file").unwrap_or_default();
